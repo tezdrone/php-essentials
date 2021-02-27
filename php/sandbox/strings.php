931ba7d0db97c64f50b9797e78ec1e9a06f8ecee
Ben <mineeraft@gmail.com>
2021-02-27 16:31:25 +1300

    Add strings

---
+++ php/sandbox/strings.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Variables</title>
+</head>
+
+<body>
+
+  <?php
+
+  echo "Hello World <br />";
+  echo 'Hello World <br/>';
+
+  $greeting = "Hello";
+  $target = "World";
+
+  $phrase = $greeting . " " . $target;
+
+  echo $phrase;
+
+  ?>
+
+  <br>
+
+  <?php
+  echo "$phrase Again </br>";
+  echo '$phrase Again </br>';
+  echo "{$phrase}Again </br>"
+  ?>
+
+
+</body>
+
+</html>
