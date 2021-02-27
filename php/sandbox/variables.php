7f6591620a89a3e5c534a532f1f6acbf628059fb
Ben <mineeraft@gmail.com>
2021-02-27 16:31:16 +1300

    Add variables

---
+++ php/sandbox/variables.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Variables</title>
+</head>
+
+<body>
+
+  <?php
+
+  $var1 = 10;
+  echo $var1;
+
+  echo '<br>';
+
+  $var1 = 100;
+  echo $var1;
+
+  echo '<br>';
+
+  $var2 = 'Hello World';
+  echo $var2;
+  ?>
+
+</body>
+
+</html>
