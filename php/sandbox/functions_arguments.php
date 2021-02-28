d38914cf25a68f8c4309936a6c18385cdbaaf722
Aurorah Harmony <mineeraft@gmail.com>
2021-03-01 10:48:10 +1300

    Add function Arguments

---
+++ php/sandbox/functions_arguments.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Functions: Arguments</title>
+</head>
+
+<body>
+  <?php
+  function say_hello_to($word)
+  {
+    echo "Hello {$word}!<br>";
+  }
+
+  say_hello_to("Everyone");
+
+  $name = "John Doe";
+  say_hello_to($name)
+
+  ?>
+
+  <?php
+  function better_hello($greeting, $target, $punct)
+  {
+    echo $greeting . " " . $target . $punct . "<br>";
+  }
+
+  better_hello("Hello", "$name", "!");
+  better_hello("Hello", "$name", null);
+  ?>
+
+</body>
+
+</html>
