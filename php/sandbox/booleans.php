651b073674e8b23ff4de5fe09b4d74eca47ea4e9
Ben <mineeraft@gmail.com>
2021-02-28 13:10:09 +1300

    Added booleans

---
+++ php/sandbox/booleans.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Booleans</title>
+</head>
+
+<body>
+  <?php
+  $result1 = true;
+  $result2 = false;
+  ?>
+
+  Result1: <?php echo $result1; ?><br>
+  Result2: <?php echo $result2; ?><br>
+
+  Result2 is boolean?: <?php echo is_bool($result2); ?><br>
+  <br>
+
+  <?php
+  $number = 3.14;
+  if (is_float($number)) {
+    echo "is Float";
+  };
+  ?>
+
+
+</body>
+
+</html>
